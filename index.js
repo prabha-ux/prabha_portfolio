4f6af16219256b501e796f7b370a25a32ef76c0a
Prabha Ganapathi <prabha.ux@gmail.com>
2019-08-25 16:41:39 -0700

    changes done after 1st revision request

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-$(document).ready(function(){
-    if($( document ).width() < 400) {
-        $('.slick-frames').slick({
-            centerMode: true,
-            variableWidth: true,
-            infinite: true,
-            slidesToShow: 2
-        });
-    }
-});
-
 function menu_toggle() {
     var x = document.getElementById("mobile_menu_bar");
     if(x.className === "menu_toggle_hide") {
