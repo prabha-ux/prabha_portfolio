618216d53d57a1fc84848366d8bcfd9066761743
Prabha Ganapathi <prabha.ux@gmail.com>
2019-08-28 23:05:44 -0700

    nav bar fixed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function menu_toggle() {
+function menu_toggle(classname) {
     var x = document.getElementById("mobile_menu_bar");
     if(x.className === "menu_toggle_hide") {
         x.className += " menu_toggle_show"
+        if(classname !== undefined) {
+            x.className += " " + classname
+        }
     } else {
         x.className = "menu_toggle_hide"
     }
